3badaaf912d46b9f6f912988e343b56e170eaced
Landry1602 <landrymaillard@gmail.com>
2024-06-24 11:33:35 +0200

    added input validation for POST and PUT

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
 const express = require("express");
 
+const { validateUser } = require("./middlewares/validateUser");
+const { validateMovie } = require("./middlewares/validateMovie");
+
 const app = express();
 
 app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
-const userControllers = require("./controllers/userControllers")
+const userControllers = require("./controllers/userControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
-app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
 
-app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
+
 
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
 
-app.post("/api/users", userControllers.postUser);
+app.post("/api/users", validateUser, userControllers.postUser);
 
-app.put("/api/users/:id", userControllers.updateUser);
+app.put("/api/users/:id", validateUser, userControllers.updateUser);
 
 module.exports = app;
--- tests/users.test.js
+++ tests/users.test.js
@@ -82,7 +82,7 @@ describe("POST /api/users", () => {
       .post("/api/users")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 });
 
@@ -146,7 +146,7 @@ describe("PUT /api/users/:id", () => {
       .post("/api/users")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 
   it("should return no user", async () => {
